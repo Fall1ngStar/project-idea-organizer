8f9edbdce81c7714f56c6e78c791395c9a4d3fb4
Fall1ngStar <titom98@live.fr>
2017-10-12 11:30:51 +0200

    cleaned code

--- index.js
+++ index.js
@@ -3,28 +3,6 @@ const app = express();
 const main_controller = require("./controllers/main_controller");
 const bodyParser = require("body-parser");
 
-/*db.sync({ force: true })
-    .then(() => {
-        return Projet.create({
-            name: "Test project",
-            uuid: uuidv4(),
-            ideas: [{
-                content: "create a web application to sotre project ideas",
-                uuid: uuidv4()
-            },
-            {
-                content: "Use nodejs, express and pugjs",
-                uuid: uuidv4()
-            },
-            {
-                content: "use sequelize for orm",
-                uuid: uuidv4()
-            }]
-        }, {
-                include: [Idea]
-            });
-    });*/
-
 app.set("view engine", "pug");
 app.use(bodyParser.json());
 app.use("/", main_controller());
@@ -32,5 +10,5 @@ app.use("/", main_controller());
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), () => {
-    console.log("Listening on port 5000");
+    console.log("Listening on port" + app.get('port'));
 });
