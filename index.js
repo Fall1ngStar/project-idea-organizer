83be2b31a1300364273fe53a75b4fdfd74242e35
Fall1ngStar <titom98@live.fr>
2017-10-12 11:24:49 +0200

    changed port again

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ app.set("view engine", "pug");
 app.use(bodyParser.json());
 app.use("/", main_controller());
 
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(80, () => {
+app.listen(app.get('port'), () => {
     console.log("Listening on port 5000");
 });
